b73fe240f4b72f068e78a5e79b8dc16372ce716c
himhike123 <himanshi@hike.in>
2020-12-05 00:03:36 +0530

    Additional time for instruments to open

--- TimeProfilerUITests/TimeProfilerUITests.swift
+++ TimeProfilerUITests/TimeProfilerUITests.swift
@@ -11,7 +11,8 @@ class TimeProfilerUITests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
-
+        XCUIApplication().launch()
+        sleep(12)
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
 
